5c27d8767b6fbd80b0cd63cd1e8751a093c7f3b5
Mitchell Vitez <mitchell@vitez.me>
2015-03-14 15:32:03 -0400

    Added styled home page

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<?php if (isset($_GET['help']) or !isset($_GET['key'])) { ?>
-    <h1>Trivium API</h1>
-    <h2>Serving up trivia questions about the University of Michigan since 2015</h2>
-    <p><a href="//vitez.me/trivium">Website</a></p>
-    <p><a href="//github.com/mitchellvitez/trivium">Github</a></p>
-    <p>For help, please first read the README.md on Github. If you are still having issues, contact <a href="mailto:mvitez@umich.edu">mvitez@umich.edu</a></p>
-    <p>To request an API key email that address. You must call the API using <code>vitez.me/trivium?key=API_KEY</code> or it will display this message</p>
-<?php
+<?php if (isset($_GET['help']) or !isset($_GET['key'])) {
+    include_once("message.php");
     exit();
 }
 
---
+++ message.php
@@ -0,0 +1,15 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h1>Trivium API</h1>
+        <h2>Serving up trivia questions about the University of Michigan since 2015</h2>
+    </div>
+    <div class="bodytext">
+        <p>Visit the <a href="//vitez.me/trivium">website</a></p>
+        <p>Check out the code on <a href="//github.com/mitchellvitez/trivium">Github</a></p>
+        <p>For help, please first read the README.md on Github. If you are still having issues, contact <a href="mailto:mvitez@umich.edu">mvitez@umich.edu</a></p>
+        <p>To request an API key email that address. You must call the API using <code>vitez.me/trivium?key=API_KEY</code> or it will display this message</p>
+    </div>
+</body>
